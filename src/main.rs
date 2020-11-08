b7ed87fbd3ee32a164a0def3d10db5501db57054
Francisco Jesus Navarro Cortes <rasprs@dancespiele.org>
2020-11-08 03:48:30 +0100

    revert main file

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,8 @@ fn main() {
     let multiples = get_multiples(2);
     // Create the `CronJob` object.
     let mut cron = CronJob::new("Dancespiele", on_cron);
-    cron.seconds(&multiples);
-    // cron.minutes(&multiples);
+    cron.seconds("0");
+    cron.minutes(&multiples);
     cron.start_job();
 }
 
